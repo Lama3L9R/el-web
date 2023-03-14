66c9ad622509ef176ab0eb0a82664db2184bfe22
Lama3L9R <i@lama.icu>
2023-03-15 00:47:26 +0800

    fix 404 in prod build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { resolve, parse } from 'path'
+import fs from 'fs'
 
 const sourceDirectory = resolve(__dirname, 'src')
 
@@ -10,6 +11,28 @@ export default defineConfig({
   build: {
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        ...(() => {
+          const scanFolder: (folder: string, accu: string[]) => void = (folder, accu) => {
+              const files = fs.readdirSync(folder).map(f => resolve(folder, f))
+
+              files.filter(f => fs.lstatSync(f).isFile()).forEach(f => accu.push(f))
+              files.filter(f => fs.lstatSync(f).isDirectory()).forEach(f => scanFolder(f, accu))
+          }
+
+          const files: string[] = []
+          scanFolder('./src', files)
+          const output: any = {}
+
+          for(const i of files.filter((it) => it.endsWith(".html"))) {
+            output[parse(i).name] = i 
+          }
+
+          return output
+        })()
+      }
+    }
   }
 })
